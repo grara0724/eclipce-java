56b36edcef424f3a5cbc629363f006a99d90fb19
grara0724 <kazuma.xx24@gmail.com>
2024-04-09 21:31:51 +0900

    コミットできてますか？

---
+++ jbasic/src/HelloJava.java
@@ -0,0 +1,12 @@
+/*
+ これはサンプルプログラム
+ 作成者:リカレントテクノロジー
+ */
+public class HelloJava {
+	public static void main(String[] args) {
+		//画面に何か表示してみます
+		System.out.println("Javaの世界へようこそ");
+		System.out.println("頑張りましょう！");
+		System.out.println("今日からJavaを頑張ります！");
+	}
+}
